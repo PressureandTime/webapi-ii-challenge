44771c4f5e352c8bc1a95c696baee5ce3a4b730a
Petar Scepanovic <hiborija@gmail.com>
2019-07-09 16:03:11 +0200

    added functionality for adding posts and comments and getting them by id

--- posts/posts-routes.js
+++ posts/posts-routes.js
@@ -15,6 +15,30 @@ route.get('/', async (req, res) => {
   }
 });
 
+route.get('/:id', async (req, res) => {
+  try {
+    const posts = await Posts.findById(req.params.id);
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the post with that ID'
+    });
+  }
+});
+
+route.get('/:id/comments', async (req, res) => {
+  try {
+    const comments = await Posts.findCommentById(req.params.id);
+    res.status(200).json(comments);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the comments'
+    });
+  }
+});
+
 route.post('/', async (req, res) => {
   try {
     const post = await Posts.insert(req.body);
@@ -23,6 +47,18 @@ route.post('/', async (req, res) => {
     console.log(error);
     res.status(500).json({
       message: 'Error adding the post'
+    });
+  }
+});
+
+route.post('/:id/comments', async (req, res) => {
+  const commentInfo = { ...req.body, post_id: req.params.id };
+  try {
+    const comment = await Posts.insertComment(commentInfo);
+    res.status(201).json(comment);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error adding the comment'
     });
   }
 });
